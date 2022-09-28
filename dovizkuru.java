db10b202cfbd378243f6b333abf22f44f0c490ae
burakkural7 <113901994+burakkural7@users.noreply.github.com>
2022-09-28 12:03:04 +0300

    Initial commit

---
+++ dovizkuru.java
@@ -0,0 +1,19 @@
+package dövizkuru;
+
+public class dovizkuru {
+
+	public static void main(String[] args) {
+		double dolar=18.45;
+		double euro=17.89;
+		
+		
+		double dovizMiktar=10;
+		String dovizTur="dolar";
+		if (dovizTur == "dolar");
+		double tlMiktar=dolar*dovizmMiktar;
+		System.out.println("Tl Miktar =" + tlMiktar);
+		double euroMiktar = tlMiktar / euro;
+		System.out.println("Euro Miktar =" + euroMiktar);
+	}
+
+}
